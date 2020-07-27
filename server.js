4822e942895da0af6523beacbfe4d550b028b304
KenFuruta <ken.furuta@37design.co.jp>
2020-07-28 08:57:47 +0900

    htmlファイルをpublicフォルダに移動

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // setup
-const serverPort = 3000;
 const comPort = 'COM11';
+var http = require("http");
 
 // file system
 const fs = require('fs');
@@ -13,24 +13,37 @@ const parsers = SerialPort.parsers;
 const parser = new parsers.Readline({
     delimiter: '\r\n'
 });
-const port = new SerialPort(comPort, {
+const comport = new SerialPort(comPort, {
     baudRate: 9600,
     parity: 'none',
     dataBits: 8,
     stopBits: 1,
     rtscts: true,
 });
-port.pipe(parser);
+comport.pipe(parser);
 
 // socket.io server setup
-var app = require('http').createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(fs.readFileSync('index.html'));
-}).listen(serverPort);
-var io = require('socket.io').listen(app);
+var server = http.createServer(function (req, res) {
+    var url = "public" + (req.url.endsWith("/") ? req.url + "index.html" : req.url);
+  if (fs.existsSync(url)) {
+    fs.readFile(url, (err, data) => {
+      if (!err) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(data);
+      }
+    });
+  }
+});
+
+var io = require('socket.io').listen(server);
+
+var port = process.env.PORT || 3000;
+server.listen(port, function() {
+    console.log("To view your app, open this link in your browser: http://localhost:" + port);
+});
 
 
-port.on('open', () => {
+comport.on('open', () => {
     console.log('Open ' + comPort);
 });
 parser.on('data', (data) => {
